71505f09a0516bba92d17724ad7bcd32139fe9f1
Evaldas Ruseckas <evaldasr@lt.ibm.com>
2017-02-08 11:11:35 +0200

    preparing release

--- src/Option.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class Option extends React.Component {
-  static propTypes = {
-    value: React.PropTypes.node,
-  };
-
-  static isSelectOption = true;
-}
